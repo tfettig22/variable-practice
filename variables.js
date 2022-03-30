81d22f0cdd5e0bab69e43831ac8db0895dc431ce
Thomas Fettig <thomasmfettig@gmail.com>
2022-03-29 20:37:46 -0600

    Add variables to file

---
+++ variables.js
@@ -0,0 +1,12 @@
+//  Declare and assign String variables
+var myName = "Tom Fettig"
+var myHometown = "Boulder Colorado"
+var myHighschool = "Fairview"
+// Declare and assign Number variables
+var myAge = 30
+var myBirthMonth = 11
+var myBirthDay = 22
+// Declare and assign Boolean variables
+var isHungry = true
+var isLearning = true
+var isSleepy = false
